e9e018bfc0d9b2384da8f7333c2ab3a5e34238b2
amit <amitmishra4893@gmail.com>
2018-04-10 09:33:28 +0530

    #live search using jquery ann ajax

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/','searchController@index');
+
+Route::get('/search','searchController@search');
